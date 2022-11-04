eeba12417169ec7124e786d94571d431135541e7
Aziiz Pranaja <pranaja200102@gmail.com>
2022-11-05 01:30:52 +0700

    added validasi monitoring

--- app/Http/Controllers/API/MonitoringController.php
+++ app/Http/Controllers/API/MonitoringController.php
@@ -20,12 +20,17 @@ class MonitoringController extends Controller
         try{
             $user = Auth::user();
             $employee = Employee::where('user_id', '=', $user->id)->first();
+            $timeNow = Carbon::now();
+            $day = Carbon::parse($timeNow);
+            $day->settings(['formatFunction' => 'translatedFormat']);
             $schedule = LessonSchedule::join('subjects', 'lesson_schedules.subject_id', '=', 'subjects.subject_id')
+                        ->join('days', 'lesson_schedules.days_id', '=', 'days.day_id')
                         ->where('teacher_id', '=', $employee->employee_id)
-                ->get([
-                    'subjects.subject_id',
-                    'subjects.subject_name'
-                ]);
+                        ->where('day_name', '=', $day->format('l'))
+                        ->get([
+                            'subjects.subject_id',
+                            'subjects.subject_name'
+                        ]);
 
             $response = $schedule;
 
@@ -43,12 +48,17 @@ class MonitoringController extends Controller
         try{
             $user = Auth::user();
             $employee = Employee::where('user_id', '=', $user->id)->first();
+            $timeNow = Carbon::now();
+            $day = Carbon::parse($timeNow);
+            $day->settings(['formatFunction' => 'translatedFormat']);
             $schedule = LessonSchedule::join('grades', 'lesson_schedules.grade_id', '=', 'grades.grade_id')
+                        ->join('days', 'lesson_schedules.days_id', '=', 'days.day_id')
                         ->where('lesson_schedules.teacher_id', '=', $employee->employee_id)
-                ->get([
-                    'grades.grade_id',
-                    'grades.grade_name'
-                ]);
+                        ->where('day_name', '=', $day->format('l'))
+                        ->get([
+                            'grades.grade_id',
+                            'grades.grade_name'
+                        ]);
 
             $response = $schedule;
 
@@ -90,20 +100,43 @@ class MonitoringController extends Controller
         }
     }
 
-    public function addMultiple(Request $request){
-        if($request->isMethod('post')){
-            $bookData = $request->all();
+    public function addMultiple(Request $request)
+    {
+        try{
+            if($request->isMethod('post')){
+                $bookData = $request->all();
+                $user = Auth::user();
+                $timeNow = Carbon::now();
+                $employee = Employee::where('user_id', '=', $user->id)->first();
+                $notScheduleDay = LessonSchedule::join('grades', 'lesson_schedules.grade_id', '=', 'grades.grade_id')
+                                ->join('days', 'lesson_schedules.days_id', '=', 'days.day_id')
+                                ->where('lesson_schedules.teacher_id', '=', $employee->employee_id)
+                                ->get();
 
-            foreach($bookData['books'] as $key => $value){
-                $book = new StudentAttendance;
-                $book->grade_id = $value['grade_id'];
-                $book->student_id = $value['student_id'];
-                $book->subject_id = $value['subject_id'];
-                $book->date = Carbon::now();
-                $book->status = $value['status'];
-                $book->save();
+                foreach($notScheduleDay as $work) {
+                    if($timeNow > $work->end_time) {
+                        return ResponseFormatter::error([], 'Jam Pelajaran Telah Usai', 400);
+                    } elseif($timeNow < $work->start_time) {
+                        return ResponseFormatter::error([], 'Jam Pelajaran Belum Mulai', 400);
+                    }
+                }
+
+                foreach($bookData['books'] as $key => $value){
+                    $book = new StudentAttendance;
+                    $book->grade_id = $value['grade_id'];
+                    $book->student_id = $value['student_id'];
+                    $book->subject_id = $value['subject_id'];
+                    $book->date = Carbon::now();
+                    $book->status = $value['status'];
+                    $book->save();
+                }
+                return ResponseFormatter::success("Succeed Absensi Siswa.");
             }
-            return ResponseFormatter::success("Succeed Absensi Siswa.");
+        }catch (Exception $e) {
+            $response = [
+                'errors' => $e->getMessage(),
+            ];
+            return ResponseFormatter::error($response, 'Something went wrong', 500);
         }
     }
 }
