fe18d38d65352986719f3f15c7c6588b7a0245a9
Aziiz Pranaja <pranaja200102@gmail.com>
2022-11-15 19:47:30 +0700

    add post penilaian

--- app/Http/Controllers/API/AssessmentController.php
+++ app/Http/Controllers/API/AssessmentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\API;
 use App\Models\Semester;
 use App\Models\Subject;
 use App\Models\Grade;
+use App\Models\Penilaian;
+use App\Models\StudentGrade;
 use App\Models\Assessment;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -100,6 +102,79 @@ class AssessmentController extends Controller
             $response = $assessment;
 
             return ResponseFormatter::success($response, 'Get Assessment Success');
+        }catch (Exception $e) {
+            $response = [
+                'errors' => $e->getMessage(),
+            ];
+            return ResponseFormatter::error($response, 'Something went wrong', 500);
+        }
+    }
+
+    public function getStudent($grade)
+    {
+        try{
+            $nilai = StudentGrade::join('students', 'student_grades.student_id', '=', 'students.student_id')
+                    ->where('student_grades.grade_id', '=', $grade)
+                    ->get([
+                        'students.student_id',
+                        'first_name',
+                        'last_name',
+                        'nisn'
+                    ]);
+
+            $fix = [];
+
+            foreach ($nilai as $n) {
+                array_push($fix, (object)["Student"=>$n,]);
+            }
+
+            $response = $fix;
+
+            return ResponseFormatter::success($response, 'Get Student Success');
+        }catch (Exception $e) {
+            $response = [
+                'errors' => $e->getMessage(),
+            ];
+            return ResponseFormatter::error($response, 'Something went wrong', 500);
+        }
+    }
+
+    public function addPenilaian(Request $request)
+    {
+        try{
+            if($request->isMethod('post')){
+                $bookData = $request->all();
+
+                foreach($bookData['books'] as $key => $value){
+                    $book = new Penilaian;
+                    $book->student_id = $value['student_id'];
+                    $book->grade_id = $value['grade_id'];
+                    $book->subject_id = $value['subject_id'];
+                    $book->assessment_id = $value['assessment_id'];
+                    $book->nilai = $value['nilai'];
+                    $book->status = "default";
+                    $book->save();
+                }
+                return ResponseFormatter::success("Succeed Penilaian Siswa.");
+            }
+        }catch (Exception $e) {
+            $response = [
+                'errors' => $e->getMessage(),
+            ];
+            return ResponseFormatter::error($response, 'Something went wrong', 500);
+        }
+    }
+
+    public function editPenilaian(Request $request, $id)
+    {
+        try{
+            $edit = [
+                "nilai" => $request->nilai
+            ];
+
+            $updateNilai = Penilaian::where('penilaian_id', '=', $id)->update($edit);
+
+            return ResponseFormatter::success('Penilaian Has Been Updated');
         }catch (Exception $e) {
             $response = [
                 'errors' => $e->getMessage(),
