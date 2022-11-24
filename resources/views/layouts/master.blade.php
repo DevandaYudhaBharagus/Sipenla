cabca55931f0b4b798e1b03bf761f16e44182a5c
farid1414 <faridputra2205@gmail.com>
2022-11-24 16:23:26 +0700

    add master role, master perpustakaan

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @include('includes.style')
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    <link rel="stylesheet" href="/css/css-internal/master.css" />
+    <link rel="icon" href="{{ asset('images/internal-images/logo.png') }}" type="image/x-icon" />
+    <title>@yield('title')</title>
+</head>
+
+<body>
+    @include('includes.sidebar')
+    <section class="home-section">
+        @include('includes.navbar-master')
+
+        <div class="content-home">
+            @yield('content')
+        </div>
+        <footer class="footer">
+            <div class="container">
+                <div class="row text-center">
+                    <div class="col-12 text-footer">
+                        &copy
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script>
+                        SIPENLA. All Rights Reserved
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </section>
+
+    @yield('modal')
+    <script>
+        window.onload = function() {
+            const sidebar = document.querySelector(".sidebar");
+            const closeBtn = document.querySelector("#btn-icon");
+
+            closeBtn.addEventListener("click", () => {
+                sidebar.classList.toggle("open");
+                btnIconChange();
+            });
+
+            function btnIconChange() {
+                if (sidebar.classList.contains("open")) {
+                    closeBtn.classList.replace(
+                        "fa-angle-double-right",
+                        "fa-angle-double-left"
+                    );
+                } else {
+                    closeBtn.classList.replace(
+                        "fa-angle-double-left",
+                        "fa-angle-double-right"
+                    );
+                }
+            }
+        };
+    </script>
+    <script>
+        const btnDropdown = document.querySelector("#dropdown-keuangan");
+        const menu = document.querySelector(".menu-dropdown");
+        btnDropdown.addEventListener("click", () => {
+            console.log("diklik");
+            if (menu.style.display == "block") {
+                menu.style.display = "none";
+            } else {
+                menu.style.display = "block";
+            }
+        });
+    </script>
+
+    @include('includes.script')
+    @stack('addon-javascript')
+
+</body>
+
+</html>
---
+++ resources/views/pages/master/master-buku-sumbangan.blade.php
@@ -0,0 +1,261 @@
+@extends('layouts.master')
+@section('title', 'Role Perpustakaan | Buku Baru')
+
+
+@section('content')
+    <div class="box-breadcrumb">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <a href="#" class="d-flex align-items-center"><i class="material-icons">home</i>
+                        Beranda</a>
+                </li>
+                <li class="breadcrumb-item" aria-current="page">Data Master</li>
+                <li class="breadcrumb-item" aria-current="page">
+                    <div class="d-flex align-items-center">
+                        <i class="fa fa-book me-1"></i> Data Perpustakaan
+                    </div>
+                </li>
+                <li class="breadcrumb-item" aria-current="page">Sumbangan</li>
+            </ol>
+        </nav>
+    </div>
+    <div class="box-content">
+        <h5>Data Buku Baru</h5>
+        <div class="d-md-flex align-items-md-center justify-content-md-between mt-2">
+            <div class="d-md-flex align-content-md-center">
+                <button class="btn-create" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Tambah Data
+                </button>
+            </div>
+            <div class="form-search">
+                <input type="search" name="" id="" placeholder="pencarian" />
+            </div>
+        </div>
+        <div class="outher-table">
+            <div class="table-scroll">
+                <table class="table-master" style="border: 1px solid black">
+                    <tr>
+                        <th width="15%">Kode Buku</th>
+                        <th width="22%">Nama Buku</th>
+                        <th width="15%">Pengarang</th>
+                        <th width="10%">Tahun</th>
+                        <th width="10%">Status</th>
+                        <th width="15%">Foto</th>
+                        <th width="145px">Aksi</th>
+                    </tr>
+                    <tr>
+                        <td width="15%">L-001</td>
+                        <td width="22%">Cerita Spiderman jadi Batman</td>
+                        <td width="15%">Azizz</td>
+                        <td width="10%">2025</td>
+                        <td width="10%">Hilang</td>
+                        <td width="15%">
+                            <div class="dropdown">
+                                <div class="btn btn-foto-master m-auto" type="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    <div class="icon-foto-master">
+                                        <img src="{{ asset('images/internal-images/foto-master.png') }}" alt="" />
+                                    </div>
+                                    <div class="fa fa-angle-down"></div>
+                                </div>
+                                <ul class="dropdown-menu dropdown-foto">
+                                    <li>
+                                        <h6>Foto</h6>
+                                        <div class="box-foto-master">
+                                            <img src="{{ asset('images/internal-images/pengumuman.jpg') }}"
+                                                alt="" />
+                                            <div class="d-flex align-items-center edit-master justify-content-end">
+                                                <a href="" class="btn-edit-master">
+                                                    <i class="fa fa-edit text-primary"></i>
+                                                </a>
+                                                <a href="" class="btn-edit-master">
+                                                    <i class="fa fa-trash-o text-danger"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </td>
+                        <td width="145px">
+                            <div class="d-flex align-items-center justify-content-center">
+                                <a href="" class="btn-edit-master me-2">
+                                    <i class="fa fa-edit text-primary"></i>
+                                </a>
+                                <a href="" class="btn-edit-master">
+                                    <i class="fa fa-trash-o text-danger"></i>
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width="15%">L-001</td>
+                        <td width="22%">Cerita Spiderman jadi Batman</td>
+                        <td width="15%">Azizz</td>
+                        <td width="10%">2025</td>
+                        <td width="10%">Hilang</td>
+                        <td width="15%">
+                            <div class="dropdown">
+                                <div class="btn btn-foto-master m-auto" type="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    <div class="icon-foto-master">
+                                        <img src="{{ asset('images/internal-images/foto-master.png') }}" alt="" />
+                                    </div>
+                                    <div class="fa fa-angle-down"></div>
+                                </div>
+                                <ul class="dropdown-menu dropdown-foto">
+                                    <li>
+                                        <h6>Foto</h6>
+                                        <div class="box-foto-master">
+                                            <img src="{{ asset('images/internal-images/pengumuman.jpg') }}"
+                                                alt="" />
+                                            <div class="d-flex align-items-center edit-master justify-content-end">
+                                                <a href="" class="btn-edit-master">
+                                                    <i class="fa fa-edit text-primary"></i>
+                                                </a>
+                                                <a href="" class="btn-edit-master">
+                                                    <i class="fa fa-trash-o text-danger"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </td>
+                        <td width="145px">
+                            <div class="d-flex align-items-center justify-content-center">
+                                <a href="" class="btn-edit-master me-2">
+                                    <i class="fa fa-edit text-primary"></i>
+                                </a>
+                                <a href="" class="btn-edit-master">
+                                    <i class="fa fa-trash-o text-danger"></i>
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width="15%">L-001</td>
+                        <td width="22%">Cerita Spiderman jadi Batman</td>
+                        <td width="15%">Azizz</td>
+                        <td width="10%">2025</td>
+                        <td width="10%">Hilang</td>
+                        <td width="15%">
+                            <div class="dropdown">
+                                <div class="btn btn-foto-master m-auto" type="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    <div class="icon-foto-master">
+                                        <img src="{{ asset('images/internal-images/foto-master.png') }}" alt="" />
+                                    </div>
+                                    <div class="fa fa-angle-down"></div>
+                                </div>
+                                <ul class="dropdown-menu dropdown-foto">
+                                    <li>
+                                        <h6>Foto</h6>
+                                        <div class="box-foto-master">
+                                            <img src="{{ asset('images/internal-images/pengumuman.jpg') }}"
+                                                alt="" />
+                                            <div class="d-flex align-items-center edit-master justify-content-end">
+                                                <a href="" class="btn-edit-master">
+                                                    <i class="fa fa-edit text-primary"></i>
+                                                </a>
+                                                <a href="" class="btn-edit-master">
+                                                    <i class="fa fa-trash-o text-danger"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </td>
+                        <td width="145px">
+                            <div class="d-flex align-items-center justify-content-center">
+                                <a href="" class="btn-edit-master me-2">
+                                    <i class="fa fa-edit text-primary"></i>
+                                </a>
+                                <a href="" class="btn-edit-master">
+                                    <i class="fa fa-trash-o text-danger"></i>
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('modal')
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-role">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5 m-auto" id="exampleModalLabel">
+                        Tambah Data Buku
+                    </h1>
+                </div>
+                <div class="modal-body">
+                    <form action="">
+                        <div class="row mb-3">
+                            <div class="col-md-6 col-12">
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Kode Buku</label>
+                                    <input type="text" class="form-control" id="nama-role" />
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-12">
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Nama Buku</label>
+                                    <input type="text" class="form-control" id="nama-role" />
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-12">
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Pengarang</label>
+                                    <input type="text" class="form-control" id="nama-role" />
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-12">
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Tahun</label>
+                                    <input type="number" class="form-control" id="nama-role" />
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-12">
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Status</label>
+                                    <input type="text" class="form-control" id="nama-role" />
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-12">
+                                <div class="mb-3">
+                                    <div class="box-image-upload-master">
+                                        <label for="" class="form-label">Foto</label>
+                                        <img src="{{ asset('images/internal-images/no-img.png') }}" alt="">
+                                        <div class="d-flex align-items-center justify-content-end edit-upload-book">
+                                            <a href="" class="btn-edit-master me-2">
+                                                <i class="fa fa-edit text-primary"></i>
+                                            </a>
+                                            <a href="" class="btn-edit-master">
+                                                <i class="fa fa-trash-o text-danger"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-permission bg-red-permission me-md-3" data-bs-dismiss="modal">
+                        Batal
+                    </button>
+                    <button type="button" class="btn btn-permission bg-green-permission">
+                        Tambah
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
