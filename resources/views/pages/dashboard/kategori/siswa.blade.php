61fe6bfff51c22e3f94301346c900bd5ff9e5985
rezakurniasetiawan <rezakurniasetiawan@gmail.com>
2022-11-25 04:16:29 +0700

    Fixing Kategori by Role

---
+++ resources/views/pages/dashboard/kategori/siswa.blade.php
@@ -0,0 +1,116 @@
+<div class="row justify-content-between mb-3" data-aos="fade-up" data-aos-delay="100">    
+    <div class="col-md-3 col-6 mb-3">
+        <a href="">
+            <div class="card-category">
+                <div class="card-body-category">
+                    <div class="card-image">
+                        <img src="{{ asset('images/internal-images/absen.png') }}" alt="" />
+                    </div>
+                    <div class="card-text">Absensi</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-md-3 col-6">
+        <a href="">
+            <div class="card-category">
+                <div class="card-body-category">
+                    <div class="card-image">
+                        <img src="{{ asset('images/internal-images/jadwal.png') }}" alt="" />
+                    </div>
+                    <div class="card-text">Jadwal</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-md-3 col-6">
+        <a href="">
+            <div class="card-category">
+                <div class="card-body-category">
+                    <div class="card-image">
+                        <img src="{{ asset('images/internal-images/penilaian.png') }}" alt="" />
+                    </div>
+                    <div class="card-text">Penilaian</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-md-3 col-6">
+        <a href="">
+            <div class="card-category">
+                <div class="card-body-category">
+                    <div class="card-image">
+                        <img src="{{ asset('images/internal-images/rapot.png') }}" alt="" />
+                    </div>
+                    <div class="card-text">Rapor</div>
+                </div>
+            </div>
+        </a>
+    </div>
+</div>
+
+<div class="row justify-content-between mb-3" data-aos="fade-up" data-aos-delay="100">
+    <div class="col-md-3 col-6 mb-3">
+        <a href="">
+            <div class="card-category">
+                <div class="card-body-category">
+                    <div class="card-image">
+                        <img src="{{ asset('images/internal-images/koperasi.png') }}" alt="" />
+                    </div>
+                    <div class="card-text">Koperasi</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-md-3 col-6">
+        <a href="">
+            <div class="card-category">
+                <div class="card-body-category">
+                    <div class="card-image">
+                        <img src="{{ asset('images/internal-images/fasilitas.png') }}" alt="" />
+                    </div>
+                    <div class="card-text">Fasilitas</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-md-3 col-6">
+        <a href="">
+            <div class="card-category">
+                <div class="card-body-category">
+                    <div class="card-image">
+                        <img src="{{ asset('images/internal-images/kantin.png') }}" alt="" />
+                    </div>
+                    <div class="card-text">Kantin</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-md-3 col-6">
+        <a href="">
+            <div class="card-category">
+                <div class="card-body-category">
+                    <div class="card-image">
+                        <img src="{{ asset('images/internal-images/keuangan.png') }}" alt="" />
+                    </div>
+                    <div class="card-text">Laporan Keuangan</div>
+                </div>
+            </div>
+        </a>
+    </div>
+</div>
+
+<div class="row mb-3" data-aos="fade-up" data-aos-delay="400">
+    <div class="col-md-3 col-6 mb-3">
+        <a href="">
+            <div class="card-category">
+                <div class="card-body-category">
+                    <div class="card-image">
+                        <img src="{{ asset('images/internal-images/perpustakaan.png') }}" alt="" />
+                    </div>
+                    <div class="card-text">Perpustakaan</div>
+                </div>
+            </div>
+        </a>
+    </div>
+</div>
