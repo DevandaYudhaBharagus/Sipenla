f4cbc1012c8a5c0fa91403e910817681be5d5000
farid1414 <faridputra2205@gmail.com>
2022-11-28 01:34:13 +0700

    add all data master

---
+++ resources/views/pages/master/master-koperasi.blade.php
@@ -0,0 +1,153 @@
+@extends('layouts.master')
+@section('title', 'Master Koperasi')
+
+
+@section('content')
+    <div class="box-breadcrumb">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <a href="#" class="d-flex align-items-center"><i class="material-icons">home</i>
+                        Beranda</a>
+                </li>
+                <li class="breadcrumb-item" aria-current="page">Data Master</li>
+                <li class="breadcrumb-item" aria-current="page">Data Koperasi</li>
+            </ol>
+        </nav>
+    </div>
+    <div class="box-content">
+        <h5>Data Koperasi</h5>
+        <div class="d-md-flex align-items-md-center justify-content-md-between mt-2">
+            <div class="d-md-flex align-content-md-center">
+                <button class="btn-create" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Tambah Data
+                </button>
+            </div>
+            <div class="form-search">
+                <input type="search" name="" id="" placeholder="pencarian" />
+            </div>
+        </div>
+        <div class="outher-table">
+            <div class="table-scroll">
+                <table class="table-master">
+                    <tr>
+                        <th width="10%">No</th>
+                        <th width="25%">Foto</th>
+                        <th width="26%">Nama Barang</th>
+                        <th width="26%">Harga</th>
+                        <th width="145px">Aksi</th>
+                    </tr>
+                    <tr>
+                        <td width="10%">1.</td>
+                        <td width="25%">
+                            <div class="dropdown">
+                                <div class="btn btn-foto-master m-auto" type="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    <div class="icon-foto-master">
+                                        <img src="{{ asset('images/internal-images/foto-master.png') }}" alt="" />
+                                    </div>
+                                    <div class="fa fa-angle-down"></div>
+                                </div>
+                                <ul class="dropdown-menu dropdown-foto">
+                                    <li>
+                                        <h6>Foto</h6>
+                                        <div class="box-foto-master">
+                                            <img src="{{ asset('images/internal-images/pengumuman.jpg') }}"
+                                                alt="" />
+                                            <div class="d-flex align-items-center edit-master justify-content-end">
+                                                <a href="" class="btn-edit-master">
+                                                    <i class="fa fa-edit text-primary"></i>
+                                                </a>
+                                                <a href="" class="btn-edit-master">
+                                                    <i class="fa fa-trash-o text-danger"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </td>
+                        <td width="26%">Dasi</td>
+                        <td width="26%">Rp 15.000</td>
+                        <td width="145px">
+                            <div class="d-flex align-items-center justify-content-center">
+                                <a href="" class="btn-edit-master me-2">
+                                    <i class="fa fa-edit text-primary"></i>
+                                </a>
+                                <a href="" class="btn-edit-master">
+                                    <i class="fa fa-trash-o text-danger"></i>
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('modal')
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-role">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5 m-auto" id="exampleModalLabel">
+                        Tambah Data Kantin
+                    </h1>
+                </div>
+                <div class="modal-body">
+                    <form action="">
+                        <div class="row mb-3">
+                            <div class="col-md-6 col-12">
+                                <input type="file" name="" id="image-master" style="display: none" multiple />
+                                <div class="mb-3">
+                                    <div class="box-image-upload-master">
+                                        <label for="" class="form-label">Foto</label>
+                                        <img src="{{ asset('images/internal-images/no-img.png') }}" alt="">
+                                        <div class="d-flex align-items-center justify-content-end edit-upload-book">
+                                            <button type="button" class="btn-edit-master me-2" onclick="uploadImage()">
+                                                <i class="fa fa-edit text-primary"></i>
+                                            </button>
+                                            <a href="" class="btn-edit-master">
+                                                <i class="fa fa-trash-o text-danger"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Nama Barang</label>
+                                    <input type="text" class="form-control" id="nama-role" />
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Harga</label>
+                                    <input type="number" class="form-control" id="nama-role" />
+                                </div>
+                            </div>
+                        </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-permission bg-red-permission me-md-3" data-bs-dismiss="modal">
+                        Batal
+                    </button>
+                    <button type="button" class="btn btn-permission bg-green-permission">
+                        Tambah
+                    </button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('addon-javascript')
+    <script>
+        function uploadImage() {
+            document.querySelector("#image-master").click();
+        }
+    </script>
+@endpush
