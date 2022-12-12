cb442ed017679023b4c30ebbdbe7f69c0d97d3c6
Aziiz Pranaja <pranaja200102@gmail.com>
2022-12-13 00:59:36 +0700

    add isi saldo api

---
+++ database/migrations/2022_12_12_212436_create_balances_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBalancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('balances', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->bigInteger('balance');
+            $table->bigInteger('remaining_balance')->nullable();
+            $table->bigInteger('used_balance')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('balances');
+    }
+}
