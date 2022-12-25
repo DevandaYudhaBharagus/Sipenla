de047933e7d3616facba85cbb40a05fd69aae8d7
farid1414 <faridputra2205@gmail.com>
2022-12-26 03:28:13 +0700

    add new regis

---
+++ resources/views/pages/penilaian/pil-penilaian.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.dashboard-layouts')
+
+@section('title', 'Penilaian')
+@section('content')
+    <div class="container">
+        <div class="box-breadcrumb">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                        <a href="/dashboard" class="d-flex align-items-center"><i class="material-icons">home</i> Beranda</a>
+                    </li>
+                    <li class="breadcrumb-item" aria-current="page">
+                        <div class="d-flex align-items-center">
+                            <img src="{{ asset('images/internal-images/icon-penilaian.png') }}"
+                                class="d-flex align-items-center me-1" width="16px" height="16px" />
+                            Penilaian Pembelajaran
+                        </div>
+                    </li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+
+    <section class="profile">
+        <div class="container">
+            <div class="box-profile ">
+                <div class="row justify-content-center schedule ">
+                    <div class="col-md-3 col-12 mb-3 mb-md-0">
+                        {{-- ini ke masukkan nilai pembelajaran --}}
+                        <a href="{{ url('/mapel-siswa') }}">
+                            <div class="box-icon-schedule">
+                                <img src="{{ asset('images/internal-images/icon-pil-penilaian.png') }}" alt="">
+                                <div class="text-blank-schedule text-center">Masukkan Nilai Pembelajaran</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3  col-12">
+                        {{-- ini ke riwayat penilaian --}}
+                        <a href="{{ url('/mapel-guru') }}">
+                            <div class="box-icon-schedule">
+                                <img src="{{ asset('images/internal-images/icon-pil-penilaian.png') }}" alt="">
+                                <div class="text-blank-schedule text-center">Riwayat Penilaian Pembelajaran</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+@endsection
