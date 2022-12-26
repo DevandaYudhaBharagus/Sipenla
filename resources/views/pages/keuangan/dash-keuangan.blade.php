30194066448eab4a504eb77503f62a6f5bfdd34e
farid1414 <faridputra2205@gmail.com>
2022-12-27 05:44:36 +0700

    add keuangan

--- resources/views/pages/keuangan/dash-keuangan.blade.php
+++ resources/views/pages/keuangan/dash-keuangan.blade.php
@@ -31,15 +31,37 @@
                     Admin
                 </div>
 
-                <div class="icon-saldo-finance">
-                    <div class="color-finance"></div>
-                    <div class="saldo-finance">
-                        <div class="text-sald">Saldo Masuk</div>
-                        <div class="price-finance">Rp 10.000.000</div>
+                <div class="d-md-flex justify-content-center mt-4">
+                    <div class="icon-saldo-finance me-3">
+                        <div class="color-finance bg-green"></div>
+                        <div class="saldo-finance">
+                            <div class="text-sald">Saldo Masuk</div>
+                            <div class="price-finance">Rp 10.000.000</div>
+                        </div>
+                    </div>
+                    <div class="icon-saldo-finance ms-3">
+                        <div class="color-finance bg-red"></div>
+                        <div class="saldo-finance">
+                            <div class="text-sald">Saldo Keluar</div>
+                            <div class="price-finance">Rp 100.000.000</div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        </div>
+            <div class="history-finance">
+                <h6>History Keuangan</h6>
+                <div class="box-history-finance">
+                    <div class="icon-history-finance">
+                        <div class="color-finance bg-green"></div>
+                        <div class="saldo-finance">
+                            <div class="text-sald">Saldo Masuk</div>
+                            <div class="price-finance">Rp 10.000.000</div>
+                        </div>
+                    </div>
+                    <div class="text-history-finance">
+                        -Rp 50.000
+                    </div>
+                </div>
+            </div>
     </section>
 @endsection
